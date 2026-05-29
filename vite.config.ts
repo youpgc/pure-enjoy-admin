06868e53e70a2ac301b8af7280e70fcbfb0b1185
Admin <admin@example.com>
2026-05-29 05:51:12 +0000

    fix: 保留console.log用于调试

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './',
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: false,
+        drop_debugger: false,
+      },
+    },
+  },
 })
