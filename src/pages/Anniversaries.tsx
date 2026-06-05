185eb7fbb175ad330741adf7569441c80f730229
PureEnjoy Dev <dev@pureenjoy.app>
2026-06-05 09:14:17 +0000

    fix: 移除Anniversaries.tsx未使用的Space导入

--- src/pages/Anniversaries.tsx
+++ src/pages/Anniversaries.tsx
@@ -1,5 +1,5 @@
 import React, { useMemo } from 'react'
-import { Space, Tag, message } from 'antd'
+import { Tag, message } from 'antd'
 import type { ColumnsType } from 'antd/es/table'
 import { EditOutlined, DeleteOutlined } from '@ant-design/icons'
 import UserDimensionList, { ModuleConfig, RecordItem } from '../components/UserDimensionList'
